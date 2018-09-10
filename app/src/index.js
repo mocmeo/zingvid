00efb4bbb42bd2c69004ecf1dc0aaad0844231f9
mocmeo <lamnnse04185@fpt.edu.vn>
2018-09-10 09:07:36 +0700

    Test: Get video link using cheerio

--- app/src/index.js
+++ app/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
+import { configureStore } from './store/configureStore';
 
 import App from './components/App';
-// import reducers from './reducers';
-
-const store = createStore(() => [], {}, applyMiddleware());
+const store = configureStore();
 
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>, 
     document.querySelector('#root'));
--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const express = require('express');
-
+const bodyParser= require('body-parser');
+const axios = require('axios');
+const cheerio = require('cheerio');
 const app = express();
-require('./routes/routes')(app);
+
+// Server configuration
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+require('./routes/mainRoutes')(app);
+
+const link = 'https://news.zing.vn/video-vuot-au-xe-may-nga-vao-gam-xe-container-post875613.html';
+const getLink = async () => {
+    const res = await axios.get(link);
+    var $ = cheerio.load(res.data);
+    const video = $('.video-player').children();
+    var title = $('.video-title').text();
+
+    console.log(video.attr('src'));
+    console.log(title);
+}
+
+getLink();
+
+
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}1!`);
+});
