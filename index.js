72e8a750bc9bb698059a6029fe0e820ac1189968
mocmeo <lamnnse04185@fpt.edu.vn>
2018-09-10 10:04:35 +0700

    Set up fetching route on server side

--- index.js
+++ index.js
@@ -15,21 +15,6 @@ app.use(function(req, res, next) {
 
 require('./routes/mainRoutes')(app);
 
-const link = 'https://news.zing.vn/video-singapore-chuan-bi-don-trung-thu-voi-den-long-rong-phuong-ruc-ro-post873952.html';
-const getLink = async () => {
-    const res = await axios.get(link);
-    var $ = cheerio.load(res.data);
-    const video = $('.video-player').children();
-    var title = $('.video-title').text();
-
-    console.log(video.attr('src'));
-    console.log(title);
-}
-
-getLink();
-
-
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}1!`);
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,6 +1,28 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
 module.exports = (app) => {
     app.get('/', (req, res) => {
         res.send('Hello world');
+    });
+
+    app.post('/', (req, res) => {
+        const { link } = req.body;
+
+        axios.get(link)
+            .then(response => {
+                let $ = cheerio.load(response.data);
+                let video = $('.video-player').children();
+                let title = $('.video-title').text();
+
+                res.send({
+                    title: title,
+                    source: video.attr('src')
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            })
     });
 
     app.get('/api', (req, res) => {
